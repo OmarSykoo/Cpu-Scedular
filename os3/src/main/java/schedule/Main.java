70405557a629ac4338ea674107e98d8814551db2
omar osama <omarosos912@gmail.com>
2024-12-03 02:48:20 +0200

    update cli

--- os3/src/main/java/schedule/Main.java
+++ os3/src/main/java/schedule/Main.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 import core.ProcessCpu;
+import core.IntervalLists.IntervalList;
+import core.Scedulers.SJFscheduler;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,6 +25,49 @@ public class Main {
             processCpu.Quantum = Integer.parseInt(inpuStrings[3]);
             processCpus.add(processCpu);
         }
+        while (true) {
+            System.out.println("1) SJF  SCHEDULAR");
+            System.out.println("2) SRTF SCHEDULAR");
+            System.out.println("3) PRIORITY SCHEDULAR");
+            System.out.println("4) FCAI SCHEDULAR");
+            System.err.println("0) Exit");
+            System.out.print("CHOOSE A SCHEDULAR : ");
+            int choice = 0;
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (Exception ex) {
+                choice = -1;
+            }
+            boolean exitFlag = false, InvalidInput = false;
+            IntervalList intervals = null;
+            switch (choice) {
+                case 0:
+                    exitFlag = true;
+                    break;
+                case 1:
+                    intervals = new SJFscheduler(processCpus).Simulate();
+                    break;
+                case 2:
+                    // add srtf later
+                    break;
+                case 3:
+                    // add priority later
+                    break;
+                case 4:
+                    // add fcai factor
+                    break;
+                default:
+                    InvalidInput = true;
+                    System.out.println("Invalid input!");
+                    break;
+            }
+            if (InvalidInput)
+                continue;
+            if (exitFlag)
+                break;
+            intervals.print();
 
+        }
+        System.out.println("Have a great day :D");
     }
 }
