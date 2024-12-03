9b7f9004988015d09fb40f6e7fdf139aa4797c8a
AnasElessily <111082600+AnasElessily@users.noreply.github.com>
2024-12-03 23:08:49 +0200

    Update PrioritySceduler.java

--- os3/src/main/java/core/Scedulers/PrioritySceduler.java
+++ os3/src/main/java/core/Scedulers/PrioritySceduler.java
@@ -1,5 +1,6 @@
 package core.Scedulers;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 
 import core.ProcessCpu;
@@ -12,17 +13,57 @@ public class PrioritySceduler extends CpuSceduler {
 
     public PrioritySceduler(LinkedList<ProcessCpu> process) {
         super(process);
-        intervals = new PriorityIntervalList();
+        this.intervals = new PriorityIntervalList();
     }
 
     @Override
     public IntervalList Simulate() {
+        process.sort(Comparator.comparingInt((ProcessCpu p) -> p.ArrivalTime));
 
-        PriorityIntervalCpu intervalCpu = new PriorityIntervalCpu();
-        intervalCpu.Pnum = 1;
-        intervalCpu.ActionDetail = "ioasjpfpa";
+        int currentTime = 0, arrivalTime = 0, burstTime = 0;
+        while (!process.isEmpty()) {
+            ProcessCpu highestPriorityProcess = null;
+            for (ProcessCpu pro : process) {
+                if (pro.ArrivalTime <= currentTime) {
+                    if (highestPriorityProcess == null || pro.Priority < highestPriorityProcess.Priority) {
+                        highestPriorityProcess = pro;
+                        arrivalTime = pro.ArrivalTime;
+                        burstTime = pro.BurstTime;
+                    }
+                }
+            }
 
-        intervals.add(intervalCpu);
+            if (highestPriorityProcess == null) {
+                currentTime++;
+                continue;
+            }
+
+            PriorityIntervalCpu intervalCpu = new PriorityIntervalCpu();
+            intervalCpu.Pnum = highestPriorityProcess.PNum;
+            intervalCpu.Priority = highestPriorityProcess.Priority;
+            intervalCpu.startTime = currentTime;
+            intervalCpu.endTime = currentTime + highestPriorityProcess.BurstTime;
+            intervalCpu.turnAroundTime = intervalCpu.endTime - arrivalTime;
+            intervalCpu.waitingTime = intervalCpu.turnAroundTime - burstTime;
+
+
+            currentTime = intervalCpu.endTime;
+
+            intervals.add(intervalCpu);
+            process.remove(highestPriorityProcess);
+        }
+
+        float avgWaiting = 0, avgTurnAround = 0;
+        for(var it : intervals) {
+            avgWaiting += it.waitingTime;
+            avgTurnAround += it.turnAroundTime;
+        }
+        avgWaiting /= intervals.size();
+        avgTurnAround /= intervals.size();
+
+        intervals.averageWaitingTime = avgWaiting;
+        intervals.averageTurnAroundTime = avgTurnAround;
+
         return intervals;
     }
 
