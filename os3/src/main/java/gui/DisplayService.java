1935da72e9d6b46b2fcc612b67956fa9ee87b671
omar osama <omarosos912@gmail.com>
2024-12-03 05:13:01 +0200

    tweak gui

--- os3/src/main/java/gui/DisplayService.java
+++ os3/src/main/java/gui/DisplayService.java
@@ -21,118 +21,119 @@ import javafx.stage.Stage;
 
 public class DisplayService extends Application {
 
-    private static IntervalList intervals;
-    private static LinkedList<ProcessCpu> process;
-    private static String[] colors;
-    private static Scene scene;
+        private static IntervalList intervals;
+        private static LinkedList<ProcessCpu> process;
+        private static String[] colors;
+        private static Scene scene;
+        private GanttChart<Number, String> ganttChart;
 
-    public static void CreateDisplay(IntervalList intervals2, LinkedList<ProcessCpu> process2) {
-        intervals = intervals2;
-        process = process2;
-        colors = new String[] {
-                "red",
-                "blue",
-                "green",
-                "yellow",
-                "purple"
-        };
-        launch();
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        AnchorPane root = new AnchorPane();
-        root.setPrefSize(1360, 889);
-
-        HBox mainHBox = new HBox(10);
-        mainHBox.setPrefSize(1360, 889);
-
-        AnchorPane leftPanel = new AnchorPane();
-
-        VBox leftVBox = new VBox(10);
-        leftVBox.setPadding(new Insets(10));
-        leftVBox.setAlignment(Pos.TOP_CENTER);
-
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setLabel("Time");
-        CategoryAxis yAxis = new CategoryAxis();
-        yAxis.setLabel("Tasks");
-
-        GanttChart<Number, String> ganttChart = new GanttChart<>(xAxis, yAxis);
-        ganttChart.setBlockHeight(60);
-        GanttChart.Series<Number, String> series = new GanttChart.Series<>();
-
-        for (IntervalCpu interval : intervals) {
-            series.getData().add(new GanttChart.Data<>(
-                    interval.startTime, "P" + interval.Pnum,
-                    new GanttChart.ExtraData(interval.executedTime(),
-                            "status-" + colors[interval.Pnum % colors.length])));
+        public static void CreateDisplay(IntervalList intervals2, LinkedList<ProcessCpu> process2) {
+                intervals = intervals2;
+                process = process2;
+                colors = new String[] {
+                                "red",
+                                "blue",
+                                "green",
+                                "yellow",
+                                "purple"
+                };
+                launch();
         }
 
-        ganttChart.getData().add(series);
-        ganttChart.setPrefSize(1007, 849);
+        @Override
+        public void start(Stage primaryStage) throws Exception {
 
-        HBox statsHBox = new HBox(12);
-        statsHBox.setAlignment(Pos.CENTER_LEFT);
-        Label statisticsLabel = new Label("Statistics");
-        statisticsLabel.setStyle("-fx-font-size: 16px; -fx-text-fill: red;");
-        Label aataLabel = new Label("AATA: 0");
-        Label awtLabel = new Label("AWT: 0");
-        statsHBox.getChildren().addAll(statisticsLabel, aataLabel, awtLabel);
+                AnchorPane root = new AnchorPane();
+                root.setPrefSize(1360, 889);
 
-        leftVBox.getChildren().addAll(ganttChart, statsHBox);
+                HBox mainHBox = new HBox(10);
+                mainHBox.setPrefSize(1360, 889);
 
-        AnchorPane.setTopAnchor(leftVBox, 0.0);
-        AnchorPane.setBottomAnchor(leftVBox, 0.0);
-        AnchorPane.setLeftAnchor(leftVBox, 0.0);
-        AnchorPane.setRightAnchor(leftVBox, 0.0);
+                AnchorPane leftPanel = new AnchorPane();
 
-        leftPanel.getChildren().add(leftVBox);
+                VBox leftVBox = new VBox(10);
+                leftVBox.setPadding(new Insets(10));
+                leftVBox.setAlignment(Pos.TOP_CENTER);
 
-        AnchorPane rightPanel = new AnchorPane();
+                NumberAxis xAxis = new NumberAxis();
+                xAxis.setLabel("Time");
+                CategoryAxis yAxis = new CategoryAxis();
+                yAxis.setLabel("Tasks");
 
-        TableView<String> processTable = new TableView<>();
-        processTable.setPrefSize(300, 858);
+                ganttChart = new GanttChart<>(xAxis, yAxis);
+                ganttChart.setBlockHeight(60);
 
-        TableColumn<String, String> processColumn = new TableColumn<>("Process");
-        TableColumn<String, String> colorColumn = new TableColumn<>("Color");
-        TableColumn<String, String> nameColumn = new TableColumn<>("Name");
-        TableColumn<String, String> pidColumn = new TableColumn<>("PID");
-        TableColumn<String, String> priorityColumn = new TableColumn<>("Priority");
+                GanttChart.Series<Number, String> series = new GanttChart.Series<>();
+                for (IntervalCpu interval : intervals) {
+                        series.getData().add(new GanttChart.Data<>(
+                                        interval.startTime, "P" + interval.Pnum,
+                                        new GanttChart.ExtraData(interval.executedTime(),
+                                                        "status-" + colors[interval.Pnum % colors.length])));
+                }
+                ganttChart.getData().add(series);
 
-        processTable.getColumns().addAll(
-                processColumn,
-                colorColumn,
-                nameColumn,
-                pidColumn,
-                priorityColumn);
+                ganttChart.setPrefSize(1007, 849);
 
-        AnchorPane.setTopAnchor(processTable, 5.0);
-        AnchorPane.setBottomAnchor(processTable, 5.0);
-        AnchorPane.setLeftAnchor(processTable, 5.0);
-        AnchorPane.setRightAnchor(processTable, 5.0);
+                HBox statsHBox = new HBox(12);
+                statsHBox.setAlignment(Pos.CENTER_LEFT);
+                Label statisticsLabel = new Label("Statistics");
+                statisticsLabel.setStyle("-fx-font-size: 16px; -fx-text-fill: red;");
+                Label aataLabel = new Label("AATA: 0");
+                Label awtLabel = new Label("AWT: 0");
+                statsHBox.getChildren().addAll(statisticsLabel, aataLabel, awtLabel);
 
-        rightPanel.getChildren().add(processTable);
+                leftVBox.getChildren().addAll(ganttChart, statsHBox);
 
-        mainHBox.getChildren().addAll(leftPanel, rightPanel);
+                AnchorPane.setTopAnchor(leftVBox, 0.0);
+                AnchorPane.setBottomAnchor(leftVBox, 0.0);
+                AnchorPane.setLeftAnchor(leftVBox, 0.0);
+                AnchorPane.setRightAnchor(leftVBox, 0.0);
 
-        AnchorPane.setTopAnchor(mainHBox, 0.0);
-        AnchorPane.setBottomAnchor(mainHBox, 0.0);
-        AnchorPane.setLeftAnchor(mainHBox, 0.0);
-        AnchorPane.setRightAnchor(mainHBox, 0.0);
+                leftPanel.getChildren().add(leftVBox);
 
-        root.getChildren().add(mainHBox);
+                AnchorPane rightPanel = new AnchorPane();
 
-        scene = new Scene(root);
-        scene.getStylesheets()
-                .add(getClass()
-                        .getResource("/gantt-chart-styles.css")
-                        .toExternalForm());
+                TableView<String> processTable = new TableView<>();
+                processTable.setPrefSize(300, 858);
 
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("JavaFX Window");
-        primaryStage.show();
-    }
+                TableColumn<String, String> processColumn = new TableColumn<>("Process");
+                TableColumn<String, String> colorColumn = new TableColumn<>("Color");
+                TableColumn<String, String> nameColumn = new TableColumn<>("Name");
+                TableColumn<String, String> pidColumn = new TableColumn<>("PID");
+                TableColumn<String, String> priorityColumn = new TableColumn<>("Priority");
+
+                processTable.getColumns().addAll(
+                                processColumn,
+                                colorColumn,
+                                nameColumn,
+                                pidColumn,
+                                priorityColumn);
+
+                AnchorPane.setTopAnchor(processTable, 5.0);
+                AnchorPane.setBottomAnchor(processTable, 5.0);
+                AnchorPane.setLeftAnchor(processTable, 5.0);
+                AnchorPane.setRightAnchor(processTable, 5.0);
+
+                rightPanel.getChildren().add(processTable);
+
+                mainHBox.getChildren().addAll(leftPanel, rightPanel);
+
+                AnchorPane.setTopAnchor(mainHBox, 0.0);
+                AnchorPane.setBottomAnchor(mainHBox, 0.0);
+                AnchorPane.setLeftAnchor(mainHBox, 0.0);
+                AnchorPane.setRightAnchor(mainHBox, 0.0);
+
+                root.getChildren().add(mainHBox);
+
+                scene = new Scene(root);
+                scene.getStylesheets()
+                                .add(getClass()
+                                                .getResource("/gantt-chart-styles.css")
+                                                .toExternalForm());
+
+                primaryStage.setScene(scene);
+                primaryStage.setTitle("JavaFX Window");
+                primaryStage.show();
+        }
 
 }
