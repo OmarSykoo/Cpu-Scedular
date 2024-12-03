890327f1f1dbe2f59f0f1c9edd0383cfd7912045
omar osama <omarosos912@gmail.com>
2024-12-03 20:23:06 +0200

    added awt atat

--- os3/src/main/java/core/IntervalLists/IntervalList.java
+++ os3/src/main/java/core/IntervalLists/IntervalList.java
@@ -7,6 +7,9 @@ import core.IntevalCpus.IntervalCpu;
 public abstract class IntervalList extends LinkedList<IntervalCpu> {
     protected abstract void printHeader();
 
+    public double averageWaitingTime = 0;
+    public double averageTurnAroundTime = 0;
+
     public void print() {
         printHeader();
         for (IntervalCpu interval : this) {
